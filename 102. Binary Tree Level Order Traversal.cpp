c0d7257473de1be54cce723aa12399a09f3b136c
V1KR4M0001 <168344287+V1KR4M0001@users.noreply.github.com>
2024-07-26 15:49:20 +0530

    Create 102. Binary Tree Level Order Traversal.cpp

---
+++ 102. Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,68 @@
+// Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+ 
+// Example 1:
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[9,20],[15,7]]
+
+// Example 2:
+// Input: root = [1]
+// Output: [[1]]
+
+// Example 3:
+// Input: root = []
+// Output: []
+ 
+
+// Constraints:
+// The number of nodes in the tree is in the range [0, 2000].
+// -1000 <= Node.val <= 1000
+
+//  Solution:=>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (root == NULL) {
+            return ans;
+        }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            int n = q.size();
+            vector<int> curntNode;
+            
+            for (int i = 0; i < n; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+                curntNode.push_back(node->val);
+                
+                if (node->left != NULL) {
+                    q.push(node->left);
+                }
+                
+                if (node->right != NULL) {
+                    q.push(node->right);
+                }
+
+            }
+            ans.push_back(curntNode);
+        }
+        
+        return ans;
+    }
+};
