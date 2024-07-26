3fc7895c61966011a9c0c4ebb1fe772422a77e07
V1KR4M0001 <168344287+V1KR4M0001@users.noreply.github.com>
2024-07-26 16:04:02 +0530

    Create 222. Count Complete Tree Nodes.cpp

---
+++ 222. Count Complete Tree Nodes.cpp
@@ -0,0 +1,50 @@
+// Given the root of a complete binary tree, return the number of the nodes in the tree.
+// According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+// Design an algorithm that runs in less than O(n) time complexity.
+
+ 
+// Example 1:
+// Input: root = [1,2,3,4,5,6]
+// Output: 6
+
+// Example 2:
+// Input: root = []
+// Output: 0
+
+// Example 3:
+// Input: root = [1]
+// Output: 1
+ 
+
+// Constraints:
+// The number of nodes in the tree is in the range [0, 5 * 104].
+// 0 <= Node.val <= 5 * 104
+// The tree is guaranteed to be complete.
+
+//  Solution:=>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int count(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
+        int ln=count(root->left);
+        int rn=count(root->right);
+        return ln+rn+1;
+    }
+    int countNodes(TreeNode* root) {
+        return count(root);
+    }
+};
